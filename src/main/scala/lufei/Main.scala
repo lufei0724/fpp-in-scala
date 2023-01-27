ccb8a9f6ed74240637ac533383b4e9214b39847e
lufei0724 <lufei0724@gmail.com>
2023-01-28 07:14:10 +1100

    Add week3 objsets

--- src/main/scala/lufei/Main.scala
+++
@@ -1,6 +0,0 @@
-package lufei
-
-import week2.funsets.FunSets.{contains, singletonSet}
-
-object Main extends App:
-  println(contains(singletonSet(1), 1))
---
+++ src/main/scala/lufei/week2/funsets/Main.scala
@@ -0,0 +1,6 @@
+package lufei.week2.funsets
+
+import lufei.week2.funsets.FunSets.{contains, singletonSet}
+
+object Main extends App:
+  println(contains(singletonSet(1), 1))
